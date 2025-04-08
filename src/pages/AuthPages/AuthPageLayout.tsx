96b9c42415419892f01d713300b281e9dd7210e3
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-04-08 16:11:34 +0700

    fix sign in

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -14,19 +14,18 @@ export default function AuthLayout({
         {children}
         <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
           <div className="relative flex items-center justify-center z-1">
-            {/* <!-- ===== Common Grid Shape Start ===== --> */}
             <GridShape />
             <div className="flex flex-col items-center max-w-xs">
               <Link to="/" className="block mb-4">
                 <img
-                  width={231}
+                  width={75}
                   height={48}
-                  src="/images/logo/auth-logo.svg"
+                  src="/images/logo/Logo.jpeg"
                   alt="Logo"
                 />
               </Link>
               <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
+                Học ngoại ngữ dễ dàng hơn bao giờ hết !{" "}
               </p>
             </div>
           </div>
