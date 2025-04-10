18b73133bb7a59a3a43cacb19d9f059bb7bdf682
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-04-10 16:29:07 +0700

    add chat by image

--- src/components/ui/images/ResponsiveImage.tsx
+++ src/components/ui/images/ResponsiveImage.tsx
@@ -1,11 +1,11 @@
-export default function ResponsiveImage() {
+export default function ResponsiveImage({ src, className }: { src: string, className?: string }) {
   return (
     <div className="relative">
       <div className="overflow-hidden">
         <img
-          src="/images/grid-image/image-01.png"
+          src={src}
           alt="Cover"
-          className="w-full border border-gray-200 rounded-xl dark:border-gray-800"
+          className={`w-full border border-gray-200 rounded-xl dark:border-gray-800 ${className}`}
         />
       </div>
     </div>
