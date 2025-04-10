5fb7076836afa86f9d167368dfcb51e5e7130bbb
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-04-10 16:33:19 +0700

    fix

--- src/pages/UiElements/Images.tsx
+++ src/pages/UiElements/Images.tsx
@@ -1,5 +1,4 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
-import ResponsiveImage from "../../components/ui/images/ResponsiveImage";
 import TwoColumnImageGrid from "../../components/ui/images/TwoColumnImageGrid";
 import ThreeColumnImageGrid from "../../components/ui/images/ThreeColumnImageGrid";
 import ComponentCard from "../../components/common/ComponentCard";
@@ -14,9 +13,6 @@ export default function Images() {
       />
       <PageBreadcrumb pageTitle="Images" />
       <div className="space-y-5 sm:space-y-6">
-        <ComponentCard title="Responsive image">
-          <ResponsiveImage />
-        </ComponentCard>
         <ComponentCard title="Image in 2 Grid">
           <TwoColumnImageGrid />
         </ComponentCard>
